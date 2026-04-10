1f7c02b61859f5e5de69657a1e9170fa4df64ac0
Christian <christian@katafract.com>
2026-04-10 19:17:32 +0000

    Fix Row.height extension causing type inference errors

--- app/src/main/java/com/katafract/safeopen/ui/ResultScreen.kt
+++ app/src/main/java/com/katafract/safeopen/ui/ResultScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
@@ -285,7 +286,3 @@ fun ResultScreen(
     }
 }
 
-@Composable
-private fun <T> Row.height(height: androidx.compose.ui.unit.Dp): Modifier {
-    return this.then(Modifier.size(height = height))
-}
